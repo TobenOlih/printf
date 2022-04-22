b644d146e0dee13c98e94d72ebb936862397ad80
nenatesz <nnennaanyadike@gmail.com>
2022-04-22 14:21:56 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -10,38 +10,38 @@
 */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	int i = 0, var = 0;
+	va_list v_ls;
+	buffer *buf;
 
-	register int count = 0;
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
+	buf = buf_new();
+	if (buf == NULL)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	if (format == NULL)
 		return (-1);
-	for (p = format; *p; p++)
+	va_start(v_ls, format);
+	while (format[i])
 	{
-		if (*p == '%')
+		buf_wr(buf);
+		if (format[i] == '%')
 		{
-			p++;
-			if (*p == '%')
+			var = opid(buf, v_ls, format, i);
+			if (var < 0)
 			{
-				count += _putchar('%');
-				continue;
+				i = var;
+				break;
 			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			i += var;
+			continue;
+		}
+		buf->str[buf->index] = format[i];
+		buf_inc(buf);
+		i++;
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	buf_write(buf);
+	if (var >= 0)
+		i = buf->overflow;
+	buf_end(buf);
+	va_end(v_ls);
+	return (i);
 }
